648390a6ad627f5273804ecf3fe1c1f9a0540f00
tonirighthere <tranvantuan842004@gmail.com>
2025-03-24 23:06:34 +0700

    Xây dựng layout chung admin

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ require("dotenv").config();
 
 const database = require("./config/database");
 
+const systemConfig = require('./config/system');
+
 const route = require("./routes/client/index.route");
 const routeAdmin = require("./routes/admin/index.route");
 
@@ -14,6 +16,9 @@ const port = process.env.PORT;
 
 app.set("views", "./views");
 app.set("view engine", "pug");
+
+// App Locals Variables
+app.locals.prefixAdmin = systemConfig.prefixAdmin;  // tạo các biến toàn cục
 
 app.use(express.static("public")); //giúp chèn các file trong public ở trong public luôn, ko phải dẫn
 
