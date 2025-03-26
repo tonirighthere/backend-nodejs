a706455be8c8cb1d35925c3c81b710b2c3932046
tonirighthere <tranvantuan842004@gmail.com>
2025-03-26 18:27:01 +0700

    Chức năng tìm kiếm

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -31,19 +31,26 @@ module.exports.index = async (req, res) => {
   }
 
   let find = {
-    deleted: false
+    deleted: false,
   };
   // nối thêm vào đối tượng
   if(req.query.status){
     find.status = req.query.status;
   }
 
+  let keyword = "";
+  if(req.query.keyword){
+    keyword = req.query.keyword;
+    const regex = new RegExp(keyword, "i");
+    find.title = regex;
+  }
   const products = await Product.find(find);
   //lấy data ở be
   // console.log(products);
   res.render("admin/pages/products/index", {  //render file và truyền biến động vào
     pageTitle: "Trang sản phẩm", 
     products: products,
-    filterStatus: filterStatus
+    filterStatus: filterStatus, 
+    keyword: keyword
   });
 }
--- public/admin/js/script.js
+++ public/admin/js/script.js
@@ -20,3 +20,24 @@ if(buttonStatus.length > 0){
   });
 }
 // End Button status
+
+
+// Form Search
+const formSearch = document.querySelector("#form-search");
+if(formSearch){
+  let url = new URL(window.location.href);
+  formSearch.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const keyword = e.target.elements.keyword.value;
+    
+    if(keyword){
+      url.searchParams.set("keyword", keyword);
+    }
+    else{
+      url.searchParams.delete("keyword");
+    }
+    window.location.href = url.href;
+  })
+}
+
+// End Form Search
